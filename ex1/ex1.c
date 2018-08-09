4835c3e5a3b0b35febad827372c66ca7672d5532
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-08-09 14:49:30 -0400

    Completed ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,24 @@
 int main(void)
 {
     // Your code here
+    int child_p = fork();
+    int x = 100;
+
+    if (child_p < 0) {
+        printf("Fork failed\n");
+        exit(1);
+    }
+    else if (child_p == 0) {
+        printf("This is CHILD process (PID: %d)\n", (int) getpid());
+        printf("Variable x in CHILD process is %d\n", x);
+        x--;
+        printf("Decremented var x in CHILD process. It's now %d\n", x);
+    } else {
+        printf("This is the PARENT process (PID: %d)\n", (int) getpid());
+        printf("Variable x in PARENT process is %d\n", x);
+        x--;
+        printf("Decremented var x in PARENT process. It's now %d\n", x);
+    }
 
     return 0;
 }

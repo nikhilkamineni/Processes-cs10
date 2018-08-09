2a2827fab6f30d1b8b61a1f30761c8dc7d63501e
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-08-09 15:47:35 -0400

    Completed ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,10 +5,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-    // Your code here 
+   FILE *file = fopen("text.txt", "a+");
+
+   int child_p = fork();
+
+   if (child_p < 0) {
+       printf("Fork failed!");
+       exit(1);
+   }
+   else if (child_p == 0) {
+        // code for child process here
+        printf("This is the CHILD process (PID: %d)\n", (int) getpid());
+        char *child_msg = "CHILD is writing to the file\n";
+        fwrite(child_msg, strlen(child_msg), 1, file);
+   }
+   else {
+       // code for parent process here
+        printf("This is the PARENT process (PID: %d)\n", (int) getpid());
+        char *parent_msg = "PARENT is writing to the file\n";
+        fwrite(parent_msg, strlen(parent_msg), 1, file);
+   }
     
+    fclose(file);
     return 0;
 }
